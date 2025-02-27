6526a32825087498cb94f8620ddcc7c7773ebd77
jordan-romeroporter <jordan.romeroporter@gmail.com>
2025-02-26 16:01:32 -0800

    Starting this project

---
+++ Rally/RallyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RallyApp.swift
+//  Rally
+//
+//  Created by Jordan Romero Porter on 2/26/25.
+//
+
+import SwiftUI
+
+@main
+struct RallyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
